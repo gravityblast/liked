411eb1f1c2cfadc873377c460299829450dd5337
Andrea Franz <andrea@gravityblast.com>
2013-10-25 01:40:46 +0100

    added delay option via env var

--- liked.js
+++ liked.js
@@ -1,12 +1,21 @@
-var Pinger = require("./pinger");
+var os     = require("os"),
+    Pinger = require("./pinger");
 
 var ids = process.argv.slice(2);
 
 if (ids < 1) {
   console.log("Usage: ");
   console.log("  liked.js PAGE_ID_1 PAGE_ID_2 [...]");
+  console.log("or: ");
+  console.log("  DELAY=5000 liked.js PAGE_ID_1 PAGE_ID_2 [...]");
   process.exit(1);
 }
 
-new Pinger(ids, 10000).start()
+var delay = parseInt(process.env.DELAY)
+if (!delay || delay == 0) {
+  delay = 30000;
+}
+
+console.log("delay: " + delay);
+new Pinger(ids, delay).start()
 
